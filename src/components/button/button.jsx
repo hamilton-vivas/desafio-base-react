062daf34cae1aac873d62eb566eb035f57564fc3
Hamilton Vivas <alextrinvivas@gmail.com>
2024-07-26 18:19:05 -0300

    Refatoração de button.jsx

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,11 +1,7 @@
 import './button.css'
 
-const showLabel = (label) => {
-   alert(`A label deste botão é: ${label}`)
-}
-
-const Button = (props) => {
-    return <button className='btn' onClick={() => showLabel(props.label)}>{props.label}</button>
-}
+ const Button = (props) => {
+     return <button className='btn' onClick={() => alert(`A label deste botão é: ${props.label}`)}>{props.label}</button>
+ }
 
 export default Button
